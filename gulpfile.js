8db5f65fbee39dd010458731efb17ef1ed3213fb
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2026-06-17 17:17:13 +0300

    did

--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,7 @@ gulp.task("html", function () {
 gulp.task("js", function () {
   return gulp
     .src("source/js/main/*.js")
-    .pipe(concat("main.js"))
+    .pipe(concat("main2.js"))
     .pipe(gulp.dest("build/js"));
 });
 
