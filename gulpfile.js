8a3184b24cca9800e505f7042fd1cbf356e6f084
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2026-07-06 20:02:02 +0300

    cache

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task("css", function () {
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
     .pipe(csso())
-    .pipe(rename("style.min2.css"))
+    .pipe(rename("style.min3.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
